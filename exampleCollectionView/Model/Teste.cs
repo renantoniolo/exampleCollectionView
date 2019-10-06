3007c1a1524cae9eac5828a8172fc35aaed2ccbb
Renan Toniolo Rocha <renantoniolo@gmail.com>
2019-10-06 16:09:49 -0300

    Primeiro Commit

---
+++ exampleCollectionView/Model/Teste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace exampleCollectionView.Model
+{
+    public class Teste
+    {
+        public string Nome { get; set; }
+
+        public string SobreNome { get; set; }
+
+        public string Idade { get; set; }
+    }
+}
---
+++ exampleCollectionView/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using exampleCollectionView.Model;
+
+namespace exampleCollectionView.ViewModel
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ObservableCollection<Teste> Testes { get; }
+
+        private int _rating;
+        public int Rating
+        {
+            get { return _rating; }
+            set => SetProperty(ref _rating, value);
+        }
+
+        public MainPageViewModel()
+        {
+            Testes = new ObservableCollection<Teste>();
+
+            Testes.Add(new Teste
+            {
+                Nome = "God of War",
+                SobreNome = "O bom da guerra"
+            });
+
+            Testes.Add(new Teste
+            {
+                Nome = "The last of us",
+                SobreNome = "Nois que sobramus"
+            });
+
+            Testes.Add(new Teste
+            {
+                Nome = "Mario Kart 8",
+                SobreNome = "Correndo com meus Amig..Inimigos"
+            });
+
+            Testes.Add(new Teste
+            {
+                Nome = "Super Mario Odyssey",
+                SobreNome = "As viagens do Bigode"
+            });
+
+            Testes.Add(new Teste
+            {
+                Nome = "Halo",
+                SobreNome = "E eu passei metiolate"
+            });
+
+        }
+    }
+}
